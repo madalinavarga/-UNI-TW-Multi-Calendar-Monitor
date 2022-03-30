97d2ce888b90f8ce6b504db0f24b13cdba3cf056
madalina varga <madalina.g.varga@gmail.com>
2022-03-30 08:40:53 +0300

    mistake branch

--- public/register.js
+++ public/register.js
@@ -1,4 +1,18 @@
 function onClick() {
-    var firstName = document.getElementById('firstName');
+    var firstName = document.getElementById('firstName')
+    var lastName = document.getElementById('lastName')
+    var email = document.getElementById('email')
+    var password = document.getElementById('password')
+    var errorElement = document.getElementById('error-msg')
+
+    let messages = []
+
+    if (firstName.value === '' || firstName.value == null) {
+        messages.push('First name is required')
+    }
+    if (lastName.value === '' || lastName.value == null) {
+        messages.push('Last name is required')
+    }
+
     console.log(firstName.value);
 }
