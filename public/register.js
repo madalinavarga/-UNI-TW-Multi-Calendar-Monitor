e9a66de30ebeade1f7301f875a8aa7da9b44bec4
madalina varga <madalina.g.varga@gmail.com>
2022-04-01 19:09:38 +0300

    Profile

--- public/register.js
+++ public/register.js
@@ -1,18 +1,17 @@
-function onClick() {
     var firstName = document.getElementById('firstName')
     var lastName = document.getElementById('lastName')
     var email = document.getElementById('email')
     var password = document.getElementById('password')
-    var errorElement = document.getElementById('error-msg')
 
-    let messages = []
+    var form = document.getElementById("form");
 
-    if (firstName.value === '' || firstName.value == null) {
-        messages.push('First name is required')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        checkPassword();
+        console.log("form");
+    });
+
+    function checkPassword() {
+        console.log("Good input");
     }
-    if (lastName.value === '' || lastName.value == null) {
-        messages.push('Last name is required')
-    }
-
-    console.log(firstName.value);
-}
