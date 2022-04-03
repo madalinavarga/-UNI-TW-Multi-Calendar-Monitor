04d5766e5ebb14349a8a84201a1e6ae0dde61a37
Delia Manu <79209225+deliamanu@users.noreply.github.com>
2022-04-04 00:18:09 +0300

    restructured folders, solved bugs and responsive footer and menu (#11)

---
+++ public/script/script.js
@@ -0,0 +1,9 @@
+const menuItems = document.querySelector(".navbar-items-container");
+
+function showMobileMenu() {
+  if (menuItems.style.display === "flex") {
+    menuItems.style.display = "none";
+  } else {
+    menuItems.style.display = "flex";
+  }
+}
