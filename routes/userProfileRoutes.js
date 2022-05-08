3137d78216743b79b48b5d7b116dcd856b5b604c
madalina varga <madalina.g.varga@gmail.com>
2022-05-08 17:17:04 +0300

    add middleware

--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const { getViewHTML } = require('../controllers/userProfileController');
+const { middleware } = require("../middleware/middleware")
 
 function userProfile(req, res) {
     switch (req.method) {
         case "GET":
-            getViewHTML(req, res);
+            //getViewHTML(req, res);
+            middleware(req, res, getViewHTML)
             break;
     }
 
