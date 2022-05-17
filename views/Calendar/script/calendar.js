6e1ccde9deb2e8be9129fb1726b80e8f28296e25
Delia Manu <79209225+deliamanu@users.noreply.github.com>
2022-05-17 13:03:14 +0300

    Calendar (#30)
    * calendar-header
    
    * finished calendar table
    
    * popup-container

--- views/Calendar/script/calendar.js
+++ views/Calendar/script/calendar.js
@@ -1,59 +1,61 @@
-let calendar;
-document.addEventListener("DOMContentLoaded", function () {
-  let calendarEl = document.getElementById("calendar");
-  calendar = new FullCalendar.Calendar(calendarEl, {
-    selectable: true,
-    initialView: "timeGridWeek",
-    headerToolbar: {
-      left: "prev,next",
-      center: "title",
-      right: "today",
-    },
-    dateClick: function (info) {
-      //   alert("Clicked on: " + info.dateStr);
-      //   alert("Coordinates: " + info.jsEvent.pageX + "," + info.jsEvent.pageY);
-      //   alert("Current view: " + info.view.type);
-      // change the day's background color just for fun
-    },
-
-    select: function (info) {
-      createCalendarEvent(info.startStr, info.endStr);
-    },
-  });
-
-  calendar.render();
-});
-
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const calendar = document.getElementById("calendar-table");
+const calendarTableHeader = document.getElementById("calendar-table-header");
 const inputDate = document.getElementById("date-picker");
 const popUp = document.querySelector(".popup-container");
-const exactTime = document.querySelector(".exact-date-time-container");
 const colorPicker = document.getElementById("color-picker-input");
+const shownWeek = document.getElementById("shown-week");
+const hour = document.getElementById("hour-cells");
+const createEvent = document.getElementById("create-event-btn");
 
-inputDate.value = new Date().toISOString().split("T")[0];
-
-const datePickerInput = (ev) => {
-  calendar.gotoDate(ev.target.value);
-};
-
-const createCalendarEvent = (startH, endH) => {
-  exactTime.style.display = "none";
-  if (popUp.style.display === "" || popUp.style.display === "none") {
-    popUp.style.display = "block";
-  }
-  const startP = document.getElementById("start-date-time");
-  const endP = document.getElementById("end-date-time");
-  const startHour = new Date(startH).toString().split(":", 2).join(":");
-  const endHour = new Date(endH).toString().split(":", 2).join(":");
-
-  startP.textContent = startHour;
-  endP.textContent = endHour;
-};
-const handleColorPicker = () => {
-  console.log(colorPicker.value);
-};
+const handleColorPicker = () => {};
 
 const handleAddEvent = () => {
-  if (popUp.style.display === "block") {
+  let startEvent = document.getElementById("start-time");
+  let endEvent = document.getElementById("end-time");
+  let dateEvent = document.getElementById("day-event");
+  let wrongInput = document.getElementById("wrong-input");
+  let ok = 1;
+  wrongInput.style.display = "none";
+  let start = startEvent.value.split(":");
+  let end = endEvent.value.split(":");
+  console.log(dateEvent.value + " " + startEvent.value + " " + endEvent.value);
+
+  if (!dateEvent.value || !startEvent.value || !endEvent.value) {
+    wrongInput.style.display = "block";
+    wrongInput.innerHTML = "Please enter the date and time";
+    ok = 0;
+  } else if (parseInt(start[0]) > parseInt(end[0])) {
+    wrongInput.style.display = "block";
+    wrongInput.innerHTML = "Check the time of the event.";
+    ok = 0;
+  } else if (parseInt(start[0]) === parseInt(end[0])) {
+    if (parseInt(end[1]) - parseInt(start[1]) < 30) {
+      wrongInput.style.display = "block";
+      wrongInput.innerHTML = "An event must be minimum half an hour.";
+      ok = 0;
+    }
+  } else if (parseInt(end[0]) - parseInt(start[0]) === 1) {
+    if (60 - parseInt(start[1]) + parseInt(end[1]) < 30) {
+      wrongInput.style.display = "block";
+      wrongInput.innerHTML = "An event must be minimum half an hour.";
+      ok = 0;
+    }
+  }
+  if (popUp.style.display === "block" && ok === 1) {
     popUp.style.display = "none";
   }
 };
@@ -63,10 +65,212 @@ const handleCreateEvent = () => {
     popUp.style.display = "none";
   } else {
     popUp.style.display = "block";
-    exactTime.style.display === "block";
   }
 };
 
 colorPicker.addEventListener("input", handleColorPicker);
 
-inputDate.addEventListener("input", datePickerInput);
+let firstDayWeek;
+let lastDayWeek;
+let curr = new Date();
+let month = curr.getMonth();
+let secondMonth = month;
+let nextMonthchanged = false;
+let year = curr.getFullYear();
+
+const getWeek = () => {
+  shownWeek.innerHTML = monthNames[month] + " " + year;
+  let first = curr.getDate() - curr.getDay() + 1;
+  let last = first + 6;
+  for (let day = first; day <= last; day++) {
+    const date = new Date(year, month, day);
+
+    const options1 = { weekday: "short" };
+    const options2 = { day: "numeric" };
+
+    let dayName = new Intl.DateTimeFormat("en-US", options1).format(date);
+    let dayNumber = new Intl.DateTimeFormat("en-US", options2).format(date);
+    calendarTableHeader.insertAdjacentHTML(
+      "beforeend",
+      `<div class="day-date"><p class="day-name">${dayName}</p><p class="day-number">${dayNumber}</p></div>`
+    );
+  }
+  firstDayWeek = last + 1;
+  lastDayWeek = firstDayWeek + 6;
+};
+let mondayStartMonth = 0;
+const getNextWeek = () => {
+  let dayNameParagraph = document.querySelectorAll(".day-name");
+  let dayNumberParagraph = document.querySelectorAll(".day-number");
+  let hourCell = document.querySelectorAll(".hour-cell");
+  let changedMonth = false;
+  let changedYear = false;
+
+  day = firstDayWeek;
+  let j = 0;
+  dayNameParagraph.forEach((element) => {
+    const date = new Date(curr.getFullYear(), curr.getMonth(), day);
+
+    if (date.getMonth() !== secondMonth && !changedMonth) {
+      secondMonth = date.getMonth();
+      changedMonth = true;
+      if (day === firstDayWeek) {
+        mondayStartMonth = 1;
+      }
+    }
+
+    if (date.getFullYear() !== year && !changedYear) {
+      year = date.getFullYear();
+      changedYear = true;
+    }
+    const options1 = { weekday: "short" };
+
+    let dayName = new Intl.DateTimeFormat("en-US", options1).format(date);
+    element.innerHTML = dayName;
+    let hourCell = document.querySelectorAll(`.hour-cell-${j}`);
+    hourCell.forEach((element) => {
+      element.id = `${date.getDate()}-${date.getMonth()}-${year}`;
+    });
+    j++;
+    day++;
+  });
+
+  day = firstDayWeek;
+  dayNumberParagraph.forEach((element) => {
+    const date = new Date(curr.getFullYear(), curr.getMonth(), day);
+
+    const options2 = { day: "numeric" };
+
+    let dayNumber = new Intl.DateTimeFormat("en-US", options2).format(date);
+    element.innerHTML = dayNumber;
+
+    day++;
+  });
+  firstDayWeek = lastDayWeek + 1;
+  lastDayWeek = firstDayWeek + 6;
+  if (secondMonth !== month && mondayStartMonth === 0) {
+    shownWeek.innerHTML =
+      monthNames[month] + " - " + monthNames[secondMonth] + " " + year;
+    month = secondMonth;
+    nextMonthchanged = true;
+  } else if (mondayStartMonth === 1) {
+    month = secondMonth;
+    mondayStartMonth = 0;
+    shownWeek.innerHTML = monthNames[month] + " " + year;
+  } else {
+    shownWeek.innerHTML = monthNames[month] + " " + year;
+  }
+};
+
+const getPreviousWeek = () => {
+  let dayNameParagraph = document.querySelectorAll(".day-name");
+  let dayNumberParagraph = document.querySelectorAll(".day-number");
+
+  let changedMonth = false;
+  let changedYear = false;
+  firstDayWeek -= 14;
+  lastDayWeek = firstDayWeek - 7;
+
+  day = firstDayWeek;
+
+  if (nextMonthchanged) {
+    month--;
+    secondMonth--;
+    nextMonthchanged = false;
+  }
+  let j = 0;
+  dayNameParagraph.forEach((element) => {
+    const date = new Date(curr.getFullYear(), curr.getMonth(), day);
+
+    if (date.getMonth() !== secondMonth && !changedMonth) {
+      secondMonth = date.getMonth();
+      console.log(secondMonth);
+      changedMonth = true;
+      if (day === lastDayWeek) {
+        mondayStartMonth = 1;
+      }
+    }
+
+    if (date.getFullYear() !== year && !changedYear) {
+      year = date.getFullYear();
+      changedYear = true;
+    }
+    const options1 = { weekday: "short" };
+
+    let dayName = new Intl.DateTimeFormat("en-US", options1).format(date);
+    element.innerHTML = dayName;
+    let hourCell = document.querySelectorAll(`.hour-cell-${j}`);
+    hourCell.forEach((element) => {
+      element.id = `${date.getDate()}-${date.getMonth()}-${year}`;
+    });
+    day++;
+    j++;
+  });
+
+  day = firstDayWeek;
+  dayNumberParagraph.forEach((element) => {
+    const date = new Date(curr.getFullYear(), curr.getMonth(), day);
+
+    const options2 = { day: "numeric" };
+
+    let dayNumber = new Intl.DateTimeFormat("en-US", options2).format(date);
+    element.innerHTML = dayNumber;
+    day++;
+  });
+  firstDayWeek += 7;
+  lastDayWeek = firstDayWeek + 6;
+  if (secondMonth !== month && mondayStartMonth === 0) {
+    shownWeek.innerHTML =
+      monthNames[secondMonth] + " - " + monthNames[month] + " " + year;
+    month = secondMonth;
+  } else if (mondayStartMonth === 1) {
+    month = secondMonth;
+    mondayStartMonth = 0;
+    shownWeek.innerHTML = monthNames[month] + " " + year;
+  } else {
+    shownWeek.innerHTML = monthNames[month] + " " + year;
+  }
+};
+
+const getHours = () => {
+  for (let i = 8; i < 13; i++) {
+    hour.insertAdjacentHTML("beforeend", `<div class="${i}-am">${i} am</div>`);
+  }
+  for (let i = 1; i < 13; i++) {
+    hour.insertAdjacentHTML("beforeend", `<div class="${i}-pm">${i} pm</div>`);
+  }
+  for (let i = 1; i < 8; i++) {
+    hour.insertAdjacentHTML("beforeend", `<div class="${i}-am">${i} am</div>`);
+  }
+};
+
+const getHourCells = () => {
+  let dayDate = document.querySelectorAll(".day-date");
+  let dayNumberParagraph = document.querySelectorAll(".day-number");
+  let j = 0;
+  dayDate.forEach((element) => {
+    for (let i = 8; i < 13; i++) {
+      element.insertAdjacentHTML(
+        "beforeend",
+        `<div class ="hour-cell-${j} ${i}am" id="${dayNumberParagraph[j].innerHTML}-${month}-${year}"></div>`
+      );
+    }
+    for (let i = 1; i < 13; i++) {
+      element.insertAdjacentHTML(
+        "beforeend",
+        `<div class ="hour-cell-${j} ${i}pm" id="${dayNumberParagraph[j].innerHTML}-${month}-${year}"></div>`
+      );
+    }
+    for (let i = 1; i < 8; i++) {
+      element.insertAdjacentHTML(
+        "beforeend",
+        `<div class ="hour-cell-${j} ${i}am" id="${dayNumberParagraph[j].innerHTML}-${month}-${year}"></div>`
+      );
+    }
+    j++;
+  });
+};
+
+getWeek();
+getHours();
+getHourCells();
