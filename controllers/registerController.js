a27153865d1530012dba76d2bdf8646379c5f0d3
varga madalina <madalina.g.varga@gmail.com>
2022-05-18 22:22:47 +0300

    moved on mongoDb

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,36 +1,37 @@
 const fs = require("fs");
-const { UserRequest, createUser } = require("../model/user")
+const bcrypt = require('bcrypt');
+const { userModel } = require("../model/user");
 
 //routes
 function getViewHTML(req, res) {
-    res.write(fs.readFileSync("./views/Register/register.html"));
+  res.write(fs.readFileSync("./views/Register/register.html"));
 }
 
 async function registerUser(req, res) {
-    return await new Promise((resolve) => {
-        let body = '';
+  return await new Promise((resolve) => {
+    let body = "";
 
-        req.on('data', function(data) {
-            body += data;
-        });
+    req.on("data", function (data) {
+      body += data;
+    });
 
-        req.on('end', async function() {
-            const data = JSON.parse(body)
-            const request = new UserRequest(data.firstName, data.lastName, data.email, data.password)
-            try {
-                await createUser(request)
-                res.writeHead(201)
-            } catch {
-                res.writeHead(500)
-            } finally {
-                resolve() //continue, no more await
-            }
-        });
-    })
+    req.on("end", async function () {
+      try {
+        const data = JSON.parse(body);
+        const encryptedPassword = await bcrypt.hash(data.password, 10);
+        await userModel.create({ ...data,password:encryptedPassword, role: 0 });
+        res.writeHead(201);
+      } catch (err) {
+        console.log(err);
+        res.writeHead(500);
+      } finally {
+        resolve(); //continue, no more await
+      }
+    });
+  });
 }
 
-
 module.exports = {
-    getViewHTML,
-    registerUser,
+  getViewHTML,
+  registerUser,
 };
--- model/index.js
+++ model/index.js
@@ -1,47 +1,13 @@
-const oracledb = require('oracledb');
-
-function getConnectionDB() {
-    return oracledb.getConnection({ user: "student", password: "STUDENT", connectionString: "localhost" });
-}
+const mongoose = require("mongoose");
 
 async function initDB() {
-    try {
-
-        let connection = await getConnectionDB();
-        console.log("Successfully connected to Oracle Database");
-        const command_users = `BEGIN
-        EXECUTE IMMEDIATE 'CREATE TABLE users
-        (id NUMBER,
-        firstName VARCHAR(20) NOT NULL,
-        lastName VARCHAR(20) NOT NULL,
-        email VARCHAR(50) NOT NULL ,
-        password VARCHAR(100) NOT NULL,
-        role NUMBER NOT NULL,
-        CONSTRAINT users_pk PRIMARY KEY (id),
-        CONSTRAINT unique_email UNIQUE(email)
-        )';
-        exception when others then
-          if SQLCODE = -955 then null; else raise; end if;
-      END;`;
-        const friends_command = `BEGIN
-        EXECUTE IMMEDIATE 'CREATE TABLE friendship
-        (user_id NUMBER , 
-        friend_id NUMBER ,
-        start_friendship date NOT NULL, 
-        CONSTRAINT friends_pk PRIMARY KEY ( user_id,friend_id ),
-        CONSTRAINT friends_fk_user FOREIGN KEY (user_id) REFERENCES users(id),
-        CONSTRAINT friends_fk_friend FOREIGN KEY (friend_id) REFERENCES users(id)
-        ) ';
-        exception when others then if SQLCODE = -955 then null; else raise; end if;
-        END;`
-        connection.execute(command_users);
-        connection.execute(friends_command);
-
-    } catch (err) {
-        console.error(err);
-    }
+  const uri =
+    "mongodb+srv://student:student1234@tw.le9r8.mongodb.net/?retryWrites=true&w=majority";
+    mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('Connected to DB!'))
+    .catch(err => console.log('DB conn error:' + err))
 }
+  
 module.exports = {
-    getConnectionDB,
-    initDB
-}
+  initDB,
+};
