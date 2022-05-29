e040abdce9aaa31c6b9bac2bf3abc78dc1525956
varga madalina <madalina.g.varga@gmail.com>
2022-05-29 12:42:26 +0300

    some changes on user profile

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const { getViewHTML } = require("../controllers/calendarController");
+const { middleware } = require("../middleware/middleware")
+
 
 function calendarRoutes(req, res) {
     switch (req.method) {
         case "GET":
-            getViewHTML(req, res);
+            middleware(req, res, getViewHTML)
             break;
         default:
             res.write('method not allowed');
